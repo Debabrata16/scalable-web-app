8609ad3299059108a5bcae9166ec7ee8948fb3bb
Debabrata16 <debabratanayakofficial1@gmail.com>
2026-02-17 10:38:20 +0530

    Add DB_PORT for Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,18 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,   
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
 db.connect(err => {
-    if(err) throw err;
-    console.log("MySQL Connected");
+    if (err) {
+        console.error("MySQL connection error:", err);
+    } else {
+        console.log("MySQL Connected");
+    }
 });
 
 module.exports = db;
